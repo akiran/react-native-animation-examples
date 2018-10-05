b731e459b85e7d6ceb55bfcb6c38f5c66f15dcca
kiran <kiran@neostack.com>
2018-10-05 20:10:11 +0530

    circle example

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 // import App from "./components/LayoutAnimations/Circle";
 // import App from "./components/Animated/Scale";
 // import App from "./components/Animated/Rotate";
-import App from "./components/Gesture/Button";
+// import App from "./components/Gesture/Button";
+import App from "./components/Gesture/PanResponderExample";
 
 export default App;
