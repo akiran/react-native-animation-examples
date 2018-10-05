62e5e336cdc1243335f6aee31767d499976061cc
kiran <kiran@neostack.com>
2018-10-05 20:31:16 +0530

    drag example

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from "react";
 // import App from "./components/Animated/Scale";
 // import App from "./components/Animated/Rotate";
 // import App from "./components/Gesture/Button";
-import App from "./components/Gesture/PanResponderExample";
+// import App from "./components/Gesture/PanResponderExample";
+import App from "./components/Gesture/Drag";
 
 export default App;
