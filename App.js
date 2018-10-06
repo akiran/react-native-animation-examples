f19ca06d02b3f6aff06ac24c0bde8f0309e6aa89
kiran <kiran@neostack.com>
2018-10-06 10:48:24 +0530

    gesture Life cycle

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import React from "react";
 // import App from "./components/Animated/Rotate";
 // import App from "./components/Gesture/Button";
 // import App from "./components/Gesture/PanResponderExample";
-import App from "./components/Gesture/Drag";
+// import App from "./components/Gesture/Drag";
 // import App from "./components/Gesture/LongPress";
 // import App from "./components/Gesture/ThreeFingerLongPress";
+// import App from "./components/Gesture/NestedView1";
+// import App from "./components/Gesture/NestedView2";
+// import App from "./components/Gesture/NestedView3";
+import App from "./components/Gesture/NestedView4";
 
 export default App;
